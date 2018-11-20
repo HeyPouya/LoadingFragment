8f8b8c7d1d08527b91f99ea30159ea85447293e2
Pouya <pouyaheydary@gmail.com>
2018-11-20 14:32:09 +0330

    added loading library

--- androidloadingfragment/src/main/java/ir/heydarii/androidloadingfragment/LoadingFragment.kt
+++ androidloadingfragment/src/main/java/ir/heydarii/androidloadingfragment/LoadingFragment.kt
@@ -1,31 +1,51 @@
 package ir.heydarii.androidloadingfragment
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.airbnb.lottie.LottieAnimationView
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val FILE_NAME = "fileName"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class LoadingFragment : Fragment() {
+open class LoadingFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_loading, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //defining lottie view
+        val lottieView = view.findViewById<LottieAnimationView>(R.id.lottie)
+
+        //getting file name
+        val fileName = Intent().getStringExtra(FILE_NAME)
+
+        //setting up lottie
+        lottieView.setAnimation(fileName)
+        lottieView.playAnimation()
+    }
+
+    companion object {
+        private var loadingFragment: LoadingFragment? = null
+        fun getInstance(fileName: String = "loading.json"): LoadingFragment? {
+            if (loadingFragment == null) {
+                loadingFragment = LoadingFragment()
+            }
+            val bundle = Bundle()
+            bundle.putString(FILE_NAME, fileName)
+            loadingFragment?.arguments = bundle
+
+            return loadingFragment
+
+        }
+    }
 
 }
